af0a403ed69bcd62c0d23b5ba756ab2c0162b9b7
Jonathan Huang <grokut@gmail.com>
2015-07-19 21:17:52 -0700

    fix/ update display page

--- src/app/components/HelloWorld.jsx
+++ src/app/components/HelloWorld.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default class HelloWorld extends React.Component {
   render(){
-    return <p>Hello World</p>
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>Now you are ready to write beautiful and modular ES6 React code!</p>
+      </div>
+    );
   }
 }
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,18 +1,15 @@
 import './styles/styles.scss';
+
 import React from 'react';
 import HelloWorld from './components/HelloWorld';
 
 export class App extends React.Component {
   render(){
-    return (
-      <div>
-        <HelloWorld/>
-      </div>
-    );
+    return <HelloWorld/>;
   }
 }
 
 React.render(
   <App />,
-  document.body
+  document.getElementById('app')
 );
